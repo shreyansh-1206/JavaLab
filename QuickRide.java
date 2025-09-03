0e2d4b5fa7570423ef8239837a44cfe46e90d9fd
Shreyansh <s.shreyansh.1206@gmail.com>
2025-09-03 11:08:18 +0530

    initial commit

---
+++ QuickRide.java
@@ -0,0 +1,11 @@
+package OOPS;
+// quick ride is launching atransport booking system
+/*books a cab for 500 with booking id cab101.
+schedules the ride at 10 am , cancels and gets 300 refund
+
+ */
+public class QuickRide {
+    public static void main(String[] args) {
+
+    }
+}
